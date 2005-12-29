85db5959abd8993fbc4d41268c028706af499e66
bshow <bshow@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2005-12-29 19:12:38 +0000

    Initial checkin

---
+++ www/doc/pages/COMMON.rb
@@ -0,0 +1,11 @@
+# Shared code for all pages below this directory.
+#
+# This is executed for every file transformed, in the binding of 
+# the appropriate Rote::Page instance. Individual page sources
+# ([pagename].rb) override anything defined here.
+#
+# Any COMMON.rb files found in directories above this will also
+# be loaded.
+
+@site_title = "Ruby libxml Project"
+layout 'normal.html'
