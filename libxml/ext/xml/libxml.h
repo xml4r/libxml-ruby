b88beb6c5fbcc51bf0449901267dcc16696dcdef
roscopeco <roscopeco@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2006-04-24 19:40:42 +0000

    release 0.3.8

--- libxml/ext/xml/libxml.h
+++ libxml/ext/xml/libxml.h
@@ -6,11 +6,11 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all. */
-#define RUBY_LIBXML_VERSION  "0.3.7"
-#define RUBY_LIBXML_VERNUM   037
-#define RUBY_LIBXML_VER_MAJ  0
-#define RUBY_LIBXML_VER_MIN  3
-#define RUBY_LIBXML_VER_MIC  7
+#define RUBY_LIBXML_VERSION  "0.3.8"
+#define RUBY_LIBXML_VERNUM   38
+#define RUBY_LIBXML_VER_MAJ   0
+#define RUBY_LIBXML_VER_MIN   3
+#define RUBY_LIBXML_VER_MIC   8
 
 #include <ruby.h>
 #include <rubyio.h>
