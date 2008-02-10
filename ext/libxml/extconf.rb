9f1cc2e7eb903da2970deea3225eecd7ebd64c37
transami <transami@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2008-02-10 16:40:08 +0000

    Added a special Windows clause to extconf.rb.

--- ext/libxml/extconf.rb
+++ ext/libxml/extconf.rb
@@ -1,98 +1,306 @@
 #!/usr/bin/env ruby
 
-require 'mkmf'
+require 'rbconfig'
 
-if defined?(CFLAGS)
-  if CFLAGS.index(CONFIG['CCDLFLAGS'])
-    $CFLAGS = CFLAGS
+def method_missing(s, *args)
+  if v = Config::CONFIG[s] || Config::CONFIG[s.upcase]
+    return v
   else
-    $CFLAGS = CFLAGS + ' ' + CONFIG['CCDLFLAGS']
+    super
   end
+end
+
+
+if RUBY_PLATFORM =~ /win32/  # FIXME: Make more robust
+
+  # We can't use Ruby's standard build procedures
+  # on Windows because the Ruby executable is
+  # built with VC++ while here we want to build
+  # with MingW.  So just roll our own...
+
+  target = "libxml_so"
+
+  #RUBY_INCLUDE_DIR = Config::CONFIG["archdir"]
+  #RUBY_BIN_DIR = Config::CONFIG["bindir"]
+  #RUBY_LIB_DIR = Config::CONFIG["libdir"]
+  #RUBY_SHARED_LIB = Config::CONFIG["LIBRUBY"]
+  #RUBY_SHARED_DLL = RUBY_SHARED_LIB.gsub(/lib$/, 'dll')
+
+  #CLEAN.include('*.o')
+  #CLOBBER.include('ruby_prof.so')
+
+  srcs = Dir['../ext/*.c']
+
+  objs = src.collect do |srcfile|
+    srcfile = File.basename(srcfile)
+    srcfile.chomp(File.extname(srcfile) + '.o')
+  end
+
+  #src.each do |srcfile|
+  #  srcfile = File.basename(srcfile)
+  #  objfile = srcfile.chomp(File.extname(srcfile) + '.o')
+  #
+  #  if File.mtime(objfile) < File.mtime(srcfile)
+  #    system "gcc -c -fPIC -O2 -Wall -o #{objfile} #{srcfile} -I#{RUBY_INCLUDE_DIR}"
+  #  end
+  #end
+
+  #system "gcc -shared -o #{LIBNAME} #{obj} #{RUBY_BIN_DIR}/#{RUBY_SHARED_DLL}"
+
+  makefile_template = DATA.read
+  makefile = eval(makefile_template)
+
+  File.open('Makefile', 'w'){ |f| f << makefile }
+
 else
-  $CFLAGS = CONFIG['CFLAGS']
-end
-$LDFLAGS = CONFIG['LDFLAGS']
-$LIBPATH.push(Config::CONFIG['libdir'])
 
-def crash(str)
-  printf(" extconf failure: %s\n", str)
-  exit 1
-end
+  require 'mkmf'
 
-dir_config('iconv')
-dir_config('xml2')
-dir_config('zlib')
+  if defined?(CFLAGS)
+    if CFLAGS.index(CONFIG['CCDLFLAGS'])
+      $CFLAGS = CFLAGS
+    else
+      $CFLAGS = CFLAGS + ' ' + CONFIG['CCDLFLAGS']
+    end
+  else
+    $CFLAGS = CONFIG['CFLAGS']
+  end
+  $LDFLAGS = CONFIG['LDFLAGS']
+  $LIBPATH.push(Config::CONFIG['libdir'])
 
-have_library('socket','socket')
-have_library('nsl','gethostbyname')
+  def crash(str)
+    printf(" extconf failure: %s\n", str)
+    exit 1
+  end
 
-unless have_library('m', 'atan')
-  # try again for gcc 4.0
-  saveflags = $CFLAGS
-  $CFLAGS += ' -fno-builtin'
+  dir_config('iconv')
+  dir_config('xml2')
+  dir_config('zlib')
+
+  have_library('socket','socket')
+  have_library('nsl','gethostbyname')
+
   unless have_library('m', 'atan')
-    crash('need libm')
+    # try again for gcc 4.0
+    saveflags = $CFLAGS
+    $CFLAGS += ' -fno-builtin'
+    unless have_library('m', 'atan')
+      crash('need libm')
+    end
+    $CFLAGS = saveflags
   end
-  $CFLAGS = saveflags
-end
 
-unless have_library('z', 'inflate') or
-       have_library('zlib', 'inflate') or
-       have_library('zlib1', 'inflate')
-  crash('need zlib')
-else
-  $defs.push('-DHAVE_ZLIB_H')
-end
+  unless have_library('z', 'inflate') or
+         have_library('zlib', 'inflate') or
+         have_library('zlib1', 'inflate')
+    crash('need zlib')
+  else
+    $defs.push('-DHAVE_ZLIB_H')
+  end
 
-unless have_library('iconv','iconv_open') or 
-       have_library('iconv','libiconv_open') or
-       have_library('libiconv', 'libiconv_open') or
-       have_library('libiconv', 'iconv_open') or
-       have_library('c','iconv_open') or
-       have_library('recode','iconv_open') or
-       have_library('iconv')
-  crash(<<EOL)
+  unless have_library('iconv','iconv_open') or 
+         have_library('iconv','libiconv_open') or
+         have_library('libiconv', 'libiconv_open') or
+         have_library('libiconv', 'iconv_open') or
+         have_library('c','iconv_open') or
+         have_library('recode','iconv_open') or
+         have_library('iconv')
+    crash(<<EOL)
 need libiconv.
 
-	Install the libiconv or try passing one of the following options
+  Install the libiconv or try passing one of the following options
 	to extconf.rb:
 
-	--with-iconv-dir=/path/to/iconv
-	--with-iconv-lib=/path/to/iconv/lib
-	--with-iconv-include=/path/to/iconv/include
+	  --with-iconv-dir=/path/to/iconv
+	  --with-iconv-lib=/path/to/iconv/lib
+	  --with-iconv-include=/path/to/iconv/include
 EOL
-end
+  end
 
-unless (have_library('xml2', 'xmlParseDoc') or
-        have_library('libxml2', 'xmlParseDoc') or
-        find_library('xml2', '/opt/lib', '/usr/local/lib', '/usr/lib')) and 
-       (have_header('libxml/xmlversion.h') or
-        find_header('libxml/xmlversion.h',
-        						"#{CONFIG['prefix']}/include", 
-                    '/opt/include/libxml2', 
-                    '/usr/local/include/libxml2', 
-                    '/usr/include/libxml2'))
-  crash(<<EOL)
+  unless (have_library('xml2', 'xmlParseDoc') or
+          have_library('libxml2', 'xmlParseDoc') or
+          find_library('xml2', '/opt/lib', '/usr/local/lib', '/usr/lib')) and 
+         (have_header('libxml/xmlversion.h') or
+          find_header('libxml/xmlversion.h',
+          						"#{CONFIG['prefix']}/include", 
+                      '/opt/include/libxml2', 
+                      '/usr/local/include/libxml2', 
+                      '/usr/include/libxml2'))
+    crash(<<EOL)
 need libxml2.
 
-        Install the library or try one of the following options to extconf.rb:
+      Install the library or try one of the following options to extconf.rb:
 
         --with-xml2-dir=/path/to/libxml2
         --with-xml2-lib=/path/to/libxml2/lib
         --with-xml2-include=/path/to/libxml2/include
 EOL
+  end
+
+  unless have_func('xmlDocFormatDump')
+    crash('Your version of libxml2 is too old.  Please upgrade.')
+  end
+
+  unless have_func('docbCreateFileParserCtxt')
+    crash('Need docbCreateFileParserCtxt')
+  end
+
+  $CFLAGS << ' ' << $INCFLAGS
+  #$INSTALLFILES = [["libxml.rb", "$(RUBYLIBDIR)", "../xml"]]
+
+  create_header()
+  #create_makefile('xml/libxml_so')
+  create_makefile('libxml_so')
+
 end
 
-unless have_func('xmlDocFormatDump')
-  crash('Your version of libxml2 is too old.  Please upgrade.')
-end
+__END__
 
-unless have_func('docbCreateFileParserCtxt')
-  crash('Need docbCreateFileParserCtxt')
-end
+SHELL = /bin/sh
 
-$CFLAGS << ' ' << $INCFLAGS
-$INSTALLFILES = [["libxml.rb", "$(RUBYLIBDIR)", "../xml"]]
+#### Start of system configuration section. ####
 
-create_header()
-create_makefile('xml/libxml_so')
+srcdir = .
+topdir = #{archdir}  #/usr/lib/ruby/1.8/x86_64-linux
+hdrdir = $(topdir)
+VPATH = $(srcdir):$(topdir):$(hdrdir)
+prefix = $(DESTDIR)/usr
+exec_prefix = $(DESTDIR)/usr
+sitedir = $(DESTDIR)/usr/local/lib/site_ruby
+rubylibdir = $(libdir)/ruby/$(ruby_version)
+docdir = $(datarootdir)/doc/$(PACKAGE)
+dvidir = $(docdir)
+datarootdir = $(prefix)/share
+archdir = $(rubylibdir)/$(arch)
+sbindir = $(exec_prefix)/sbin
+psdir = $(docdir)
+localedir = $(datarootdir)/locale
+htmldir = $(docdir)
+datadir = $(datarootdir)
+includedir = $(prefix)/include
+infodir = $(prefix)/share/info
+sysconfdir = $(DESTDIR)/etc
+mandir = $(prefix)/share/man
+libdir = $(DESTDIR)/usr/lib
+sharedstatedir = $(prefix)/com
+oldincludedir = $(DESTDIR)/usr/include
+pdfdir = $(docdir)
+sitearchdir = $(sitelibdir)/$(sitearch)
+bindir = $(exec_prefix)/bin
+localstatedir = $(DESTDIR)/var
+sitelibdir = $(sitedir)/$(ruby_version)
+libexecdir = $(prefix)/lib/ruby1.8
+
+CC = cc
+LIBRUBY = $(LIBRUBY_SO)
+LIBRUBY_A = lib$(RUBY_SO_NAME)-static.a
+LIBRUBYARG_SHARED = -l$(RUBY_SO_NAME)
+LIBRUBYARG_STATIC = -l$(RUBY_SO_NAME)-static
+
+RUBY_EXTCONF_H = extconf.h
+CFLAGS   =  -fPIC -fno-strict-aliasing -g -O2  -fPIC -I. -I/usr/lib/ruby/1.8/x86_64-linux -I. -I/usr/include/libxml2 
+INCFLAGS = -I. -I. -I/usr/lib/ruby/1.8/x86_64-linux -I. -I/usr/include/libxml2
+CPPFLAGS = -DRUBY_EXTCONF_H=\"$(RUBY_EXTCONF_H)\" 
+CXXFLAGS = $(CFLAGS) 
+DLDFLAGS = -L.  -rdynamic -Wl,-export-dynamic  
+LDSHARED = $(CC) -shared
+AR = ar
+EXEEXT = 
+
+RUBY_INSTALL_NAME = ruby1.8
+RUBY_SO_NAME = ruby1.8
+arch = x86_64-linux
+sitearch = x86_64-linux
+ruby_version = 1.8
+ruby = /usr/bin/ruby1.8
+RUBY = $(ruby)
+RM = rm -f
+MAKEDIRS = mkdir -p
+INSTALL = /usr/bin/install -c
+INSTALL_PROG = $(INSTALL) -m 0755
+INSTALL_DATA = $(INSTALL) -m 644
+COPY = cp
+
+preload = 
+
+libpath = . $(libdir) /usr/lib
+LIBPATH =  -L"." -L"$(libdir)" -L"/usr/lib"
+DEFFILE = 
+
+CLEANFILES = 
+DISTCLEANFILES = 
+
+extout = 
+extout_prefix = 
+target_prefix = /xml
+LOCAL_LIBS = 
+LIBS = $(LIBRUBYARG_SHARED) -lxml2 -lc -lz -lm -lnsl  -lpthread -ldl -lcrypt -lm   -lc
+
+SRCS = #{srcs.join(' ')}
+OBJS = #{objs.join(' ')}
+
+TARGET = #{target}
+DLLIB = $(TARGET).so
+EXTSTATIC = 
+STATIC_LIB = 
+
+RUBYCOMMONDIR = $(sitedir)$(target_prefix)
+RUBYLIBDIR    = $(sitelibdir)$(target_prefix)
+RUBYARCHDIR   = $(sitearchdir)$(target_prefix)
+
+TARGET_SO     = $(DLLIB)
+CLEANLIBS     = $(TARGET).so $(TARGET).il? $(TARGET).tds $(TARGET).map
+CLEANOBJS     = *.o *.a *.s[ol] *.pdb *.exp *.bak
+
+all:		$(DLLIB)
+static:		$(STATIC_LIB)
+
+clean:
+		@-$(RM) $(CLEANLIBS) $(CLEANOBJS) $(CLEANFILES)
+
+distclean:	clean
+		@-$(RM) Makefile $(RUBY_EXTCONF_H) conftest.* mkmf.log
+		@-$(RM) core ruby$(EXEEXT) *~ $(DISTCLEANFILES)
+
+realclean:	distclean
+install: install-so install-rb
+
+install-so: $(RUBYARCHDIR)
+install-so: $(RUBYARCHDIR)/$(DLLIB)
+$(RUBYARCHDIR)/$(DLLIB): $(DLLIB)
+	$(INSTALL_PROG) $(DLLIB) $(RUBYARCHDIR)
+install-rb: pre-install-rb install-rb-default
+install-rb-default: pre-install-rb-default
+pre-install-rb: Makefile
+pre-install-rb-default: Makefile
+$(RUBYARCHDIR):
+	$(MAKEDIRS) $@
+
+site-install: site-install-so site-install-rb
+site-install-so: install-so
+site-install-rb: install-rb
+
+.SUFFIXES: .c .m .cc .cxx .cpp .C .o
+
+.cc.o:
+	$(CXX) $(INCFLAGS) $(CPPFLAGS) $(CXXFLAGS) -c $<
+
+.cxx.o:
+	$(CXX) $(INCFLAGS) $(CPPFLAGS) $(CXXFLAGS) -c $<
+
+.cpp.o:
+	$(CXX) $(INCFLAGS) $(CPPFLAGS) $(CXXFLAGS) -c $<
+
+.C.o:
+	$(CXX) $(INCFLAGS) $(CPPFLAGS) $(CXXFLAGS) -c $<
+
+.c.o:
+	$(CC) $(INCFLAGS) $(CPPFLAGS) $(CFLAGS) -c $<
+
+$(DLLIB): $(OBJS)
+	@-$(RM) $@
+	$(LDSHARED) -o $@ $(OBJS) $(LIBPATH) $(DLDFLAGS) $(LOCAL_LIBS) $(LIBS)
+
+
+$(OBJS): ruby.h defines.h $(RUBY_EXTCONF_H)
+
