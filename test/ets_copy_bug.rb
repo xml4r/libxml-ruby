07b9f9919920fc81fdf25ac8aa21f546e0932719
cfis <cfis@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2008-07-09 08:56:52 +0000

    Updated tests based on reported bugs.

--- test/ets_copy_bug.rb
+++ test/ets_copy_bug.rb
@@ -4,7 +4,7 @@ def test( doc2 )
   doc = XML::Document.new('1.0')
   doc.root = XML::Node.new("ccc")
   doc.root['aaa'] = 'aaa'
-  doc.root.child_add(doc2.root) # BUG!
+  doc.root.child_add(doc2.root.copy(true)) # BUG!
   doc.root << doc2.root.copy(true)
   return doc
 end
--- test/tc_xml_xpath.rb
+++ test/tc_xml_xpath.rb
@@ -4,37 +4,50 @@ require "test/unit"
 
 class TextXPath < Test::Unit::TestCase
   def setup()
-    xp = XML::Parser.new()
-    str = '<ruby_array uga="booga" foo="bar"><fixnum>one</fixnum><fixnum>two</fixnum></ruby_array>'
-    assert_equal(str, xp.string = str)
-    doc = xp.parse
-    assert_instance_of(XML::Document, doc)
-    @xpt = doc.find('/ruby_array/fixnum')
-    assert_instance_of(XML::XPath::Object, @xpt)
+    @doc = XML::Parser.string(<<-EOS).parse
+      <ruby_array uga="booga" foo="bar">
+        <fixnum>one</fixnum>
+        <fixnum>two</fixnum>
+      </ruby_array>
+    EOS
   end
 
   def teardown()
-    @xpt = nil
+    @doc = nil
   end
 
-  def test_libxml_xpath_set()
-    set = @xpt.set
-    assert_instance_of(XML::Node::Set, set)
+  def test_find_class
+    nodes = @doc.find('/ruby_array/fixnum')
+    assert_instance_of(XML::XPath::Object, nodes)
   end
 
-  def test_ary()
-    assert_equal(2,@xpt.length)
-    assert_equal("one",@xpt.first.content)
-    assert_equal("one",@xpt[0].content)
-    assert_equal("two",@xpt[1].content)
-    assert_equal("two",@xpt[-1].content)
-    assert_equal(nil,@xpt[-3])
-    assert_equal(nil,@xpt[2])
+  def test_xpath_set
+    nodes = @doc.find('/ruby_array/fixnum')
+    assert_instance_of(XML::Node::Set, nodes.set)
+  end
+  
+  def test_xpath_empty
+    nodes = @doc.find('/ruby_array/i_dont_exist')
+    assert_equal(0, nodes.length)
+    
+    node = nodes.first
+    assert_nil(node)
+  end    
 
-    @xpt.inject(%w(one two).reverse) {|m,v|
+  def test_ary
+    nodes = @doc.find('/ruby_array/fixnum')
+    assert_equal(2, nodes.length)
+    assert_equal("one", nodes.first.content)
+    assert_equal("one", nodes[0].content)
+    assert_equal("two", nodes[1].content)
+    assert_equal("two", nodes[-1].content)
+    assert_equal(nil, nodes[-3])
+    assert_equal(nil, nodes[2])
+
+    nodes.inject(%w(one two).reverse) do |m,v|
       assert_equal(m.pop,v.content)
       m
-    }
+    end
   end
   
   def test_custom_function
