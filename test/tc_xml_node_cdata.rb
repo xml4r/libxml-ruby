74c79516b64ae227ed9bc84d07ebdb5845fa9d3c
cfis <cfis@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2008-07-19 08:39:53 +0000

    Beefed up CData tests in response to reported bug.

--- test/tc_xml_node_cdata.rb
+++ test/tc_xml_node_cdata.rb
@@ -1,8 +1,8 @@
-require "xml"
+require 'xml'
 require 'test/unit'
 
 class CDataCommentTest < Test::Unit::TestCase
-  def setup()
+  def setup
     xp = XML::Parser.new()
     str = '<root></root>'
     assert_equal(str, xp.string = str)
@@ -11,22 +11,41 @@ class CDataCommentTest < Test::Unit::TestCase
     @root = @doc.root
   end
 
-  def test_libxml_node_add_cdata_01
+  def test_node_type
+    cnode = XML::Node.new_cdata('test cdata')
+    assert_equal(XML::Node::CDATA_SECTION_NODE, cnode.node_type)
+  end
+
+  def test_add_cdata
     @root << XML::Node.new_cdata('mycdata')
     assert_equal '<root><![CDATA[mycdata]]></root>',
       @root.to_s.gsub(/\n\s*/,'')
   end
 
-  def test_libxml_node_add_cdata_02
+  def test_add_cdata_2
     @root << XML::Node.new_cdata('mycdata')
     assert_equal 'cdata',
     @root.child.node_type_name
   end
 
-  def test_libxml_node_add_cdata_03
+  def test_add_cdata_3
     @root << el = XML::Node.new_cdata('mycdata')
     el << "_this_is_added"
     assert_equal '<root><![CDATA[mycdata_this_is_added]]></root>',
       @root.to_s.gsub(/\n\s*/,'')
+  end
+  
+  def test_attributes
+    cnode = XML::Node.new_cdata('test cdata')
+    assert_equal(0, cnode.attributes.length)
+  end
+  
+  def test_set_cdata_attribute
+    cnode = XML::Node.new_cdata('test cdata')
+    
+    # Can't create attributes on non-element nodes
+    assert_raise(ArgumentError) do
+      cnode['attr'] = '123'
+    end
   end
 end
