4930864af37f24f8c24991a5fd6be62424a4498f
cfis <cfis@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2008-07-22 02:11:00 +0000

    Check in windows path hack.

--- lib/libxml.rb
+++ lib/libxml.rb
@@ -1,6 +1,13 @@
 # $Id$ 
 # Please see the LICENSE file for copyright and distribution information 
 
+# If running on Windows, then add the current directory to the PATH
+# for the current process so it can find the pre-built libxml2 and 
+# iconv2 shared libraries (dlls).
+if RUBY_PLATFORM.match(/mswin/i)
+  ENV['PATH'] += ";#{File.dirname(__FILE__)}"
+end
+
 # Load the C-based binding.
 require 'libxml_ruby'
 
