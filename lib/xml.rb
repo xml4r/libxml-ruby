0c4baad16eb14338e2ef13ea234d5873a53eecc9
cfis <cfis@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2008-07-22 04:39:58 +0000

    Messed up .inc for c files.  Add back in and a fix to the readme file.

--- lib/xml.rb
+++ lib/xml.rb
@@ -1,9 +1,9 @@
-# This file loads libxml and add the LibXML namespace
+# This file loads libxml and adds the LibXML namespace
 # to the toplevel for conveneience. The end result
 # is to have XML:: universally exposed.
 #
 # It is recommend that you only load this file for libs
-# that do not have there own namespace, eg. administrative
+# that do not have their own namespace, eg. administrative
 # scripts, personal programs, etc. For other applications
 # require 'libxml' instead and include LibXML into your 
 # app/libs namespace.
