985b0b91059ce80c090225c17845dcb6fe833df3
cfis <cfis@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2008-11-16 01:18:42 +0000

    Fix applied from Michael Guterl.

--- test/tc_ns.rb
+++ test/tc_ns.rb
@@ -5,8 +5,8 @@ class TestNS < Test::Unit::TestCase
   def test_ns
     node = XML::Node.new('foo')
     ns = XML::NS.new(node, 'http://www.mynamespace.com', 'my_namepace')
-    assert(ns.prefix, 'my_namepace')
-    assert(ns.href, 'http://www.mynamespace.com')
+    assert_equal(ns.prefix, 'my_namepace')
+    assert_equal(ns.href, 'http://www.mynamespace.com')
   end
   
   def test_default_ns
