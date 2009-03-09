f53173ec2fafd8714a18df6a2e1419594a3c51c7
cfis <cfis@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2009-03-09 17:46:29 +0000

    Version 1.1.0

--- ext/libxml/ruby_xml_version.h
+++ ext/libxml/ruby_xml_version.h
@@ -1,9 +1,9 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all.*/
-#define RUBY_LIBXML_VERSION  "1.0.0"
-#define RUBY_LIBXML_VERNUM   100
+#define RUBY_LIBXML_VERSION  "1.1.0"
+#define RUBY_LIBXML_VERNUM   110
 #define RUBY_LIBXML_VER_MAJ   1
-#define RUBY_LIBXML_VER_MIN   0
+#define RUBY_LIBXML_VER_MIN   1
 #define RUBY_LIBXML_VER_MIC   0
 #define RUBY_LIBXML_VER_PATCH 0
