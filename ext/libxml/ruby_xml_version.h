5c3d2f6620c238830f4afcedf6b85a1ccc5a7748
cfis <cfis@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2009-03-10 18:40:24 +0000

    Updated version to 1.1.1 and new windows binaries.

--- ext/libxml/ruby_xml_version.h
+++ ext/libxml/ruby_xml_version.h
@@ -1,8 +1,8 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all.*/
-#define RUBY_LIBXML_VERSION  "1.1.0"
-#define RUBY_LIBXML_VERNUM   110
+#define RUBY_LIBXML_VERSION  "1.1.1"
+#define RUBY_LIBXML_VERNUM   111
 #define RUBY_LIBXML_VER_MAJ   1
 #define RUBY_LIBXML_VER_MIN   1
 #define RUBY_LIBXML_VER_MIC   0
