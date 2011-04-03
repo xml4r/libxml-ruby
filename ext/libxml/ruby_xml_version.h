674a609469cdc2f46cfa3b2b4c417e4227c9f521
Charlie Savage <cfis@zerista.com>
2011-04-03 02:54:56 -0600

    Bump version to 1.1.5

--- ext/libxml/ruby_xml_version.h
+++ ext/libxml/ruby_xml_version.h
@@ -1,8 +1,8 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all.*/
-#define RUBY_LIBXML_VERSION  "1.1.4"
-#define RUBY_LIBXML_VERNUM   114
+#define RUBY_LIBXML_VERSION  "1.1.5"
+#define RUBY_LIBXML_VERNUM   115
 #define RUBY_LIBXML_VER_MAJ   1
 #define RUBY_LIBXML_VER_MIN   1
 #define RUBY_LIBXML_VER_MIC   4
