885b571da5f58ce862c66704e70683b531cefbc7
Charlie Savage <cfis@zerista.com>
2011-04-22 22:50:19 -0600

    Remove commented out, old code.

--- ext/libxml/ruby_xml_schema.c
+++ ext/libxml/ruby_xml_schema.c
@@ -99,61 +99,10 @@ static VALUE rxml_schema_init_from_string(VALUE self, VALUE schema_str)
   return Data_Wrap_Struct(cXMLSchema, NULL, rxml_schema_free, xschema);
 }
 
-/* TODO what is this patch doing here?
-
- xmlSchemaParserCtxtPtr  parser;
- xmlSchemaPtr            sptr;
- xmlSchemaValidCtxtPtr   vptr;
- +	int                     is_invalid;
-
- if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z", &source) == FAILURE) {
- return;
- @@ -598,26 +598,24 @@
- convert_to_string_ex(&source);
- parser = xmlSchemaNewParserCtxt(Z_STRVAL_P(source));
- sptr = xmlSchemaParse(parser);
- break;
- case SCHEMA_BLOB:
- convert_to_string_ex(&source);
- parser = xmlSchemaNewMemParserCtxt(Z_STRVAL_P(source), Z_STRLEN_P(source));
- sptr = xmlSchemaParse(parser);
- break;
- }
-
- vptr = xmlSchemaNewValidCtxt(sptr);
- +	is_invalid = xmlSchemaValidateDoc(vptr, (xmlDocPtr) sxe->document->ptr);
- xmlSchemaFree(sptr);
- xmlSchemaFreeValidCtxt(vptr);
- xmlSchemaFreeParserCtxt(parser);
-
- -	if (is_valid) {
- -		RETURN_TRUE;
- -	} else {
- +	if (is_invalid) {
- RETURN_FALSE;
- +	} else {
- +		RETURN_TRUE;
- }
- }
- }}}
- @@ -695,7 +693,7 @@
- {
- if (!strcmp(method, "xsearch")) {
- simplexml_ce_xpath_search(INTERNAL_FUNCTION_PARAM_PASSTHRU);
- -#ifdef xmlSchemaParserCtxtPtr
- +#ifdef LIBXML_SCHEMAS_ENABLED
- } else if (!strcmp(method, "validate_schema_file")) {
- simplexml_ce_schema_validate(INTERNAL_FUNCTION_PARAM_PASSTHRU, SCHEMA_FILE);
- } else if (!strcmp(method, "validate_schema_buffer")) {
- */
-
 void rxml_init_schema(void)
 {
   cXMLSchema = rb_define_class_under(mXML, "Schema", rb_cObject);
   rb_define_singleton_method(cXMLSchema, "new", rxml_schema_init_from_uri, 1);
-  rb_define_singleton_method(cXMLSchema, "from_string",
-      rxml_schema_init_from_string, 1);
-  rb_define_singleton_method(cXMLSchema, "document",
-      rxml_schema_init_from_document, 1);
+  rb_define_singleton_method(cXMLSchema, "from_string", rxml_schema_init_from_string, 1);
+  rb_define_singleton_method(cXMLSchema, "document", rxml_schema_init_from_document, 1);
 }
-
