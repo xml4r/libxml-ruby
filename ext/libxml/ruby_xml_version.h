5f475e6d55a443529e2f186d6c0ec63193d9839a
Charlie Savage <cfis@zerista.com>
2011-05-01 19:45:44 -0400

    Updated version to 2.0.3.

--- ext/libxml/ruby_xml_version.h
+++ ext/libxml/ruby_xml_version.h
@@ -1,9 +1,9 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all.*/
-#define RUBY_LIBXML_VERSION  "2.0.2"
-#define RUBY_LIBXML_VERNUM   202
+#define RUBY_LIBXML_VERSION  "2.0.3"
+#define RUBY_LIBXML_VERNUM   203
 #define RUBY_LIBXML_VER_MAJ   2
 #define RUBY_LIBXML_VER_MIN   0
-#define RUBY_LIBXML_VER_MIC   2
+#define RUBY_LIBXML_VER_MIC   3
 #define RUBY_LIBXML_VER_PATCH 0
