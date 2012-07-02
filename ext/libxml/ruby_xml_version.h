32bd51104ff8aab0c49602252d7b7c07012f3ee1
Charlie Savage <cfis@zerista.com>
2012-07-01 17:51:37 -0700

    Versoin 2.3.3.

--- ext/libxml/ruby_xml_version.h
+++ ext/libxml/ruby_xml_version.h
@@ -1,9 +1,9 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all.*/
-#define RUBY_LIBXML_VERSION  "2.3.2"
-#define RUBY_LIBXML_VERNUM   232
+#define RUBY_LIBXML_VERSION  "2.3.3"
+#define RUBY_LIBXML_VERNUM   233
 #define RUBY_LIBXML_VER_MAJ   2
 #define RUBY_LIBXML_VER_MIN   3
-#define RUBY_LIBXML_VER_MIC   2
+#define RUBY_LIBXML_VER_MIC   3
 #define RUBY_LIBXML_VER_PATCH 0
