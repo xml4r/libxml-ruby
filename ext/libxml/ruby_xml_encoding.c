1f6ae4483217ff51fefd536bcc3dedfc94d55975
Nikita Afanasenko <nikita@afanasenko.name>
2012-08-25 13:12:02 +0400

    Update encoding conversion functions docs.

--- ext/libxml/ruby_xml_encoding.c
+++ ext/libxml/ruby_xml_encoding.c
@@ -41,7 +41,7 @@ VALUE mXMLEncoding;
 
 /*
  * call-seq:
- *    Input.s_to_encoding("UTF_8") -> XML::Encoding::UTF_8
+ *    Encoding.from_s("UTF_8") -> XML::Encoding::UTF_8
  *
  * Converts an encoding string to an encoding constant
  * defined on the XML::Encoding class.
@@ -59,7 +59,7 @@ static VALUE rxml_encoding_from_s(VALUE klass, VALUE encoding)
 
 /*
  * call-seq:
- *    Input.encoding_to_s(Input::ENCODING) -> "encoding"
+ *    Encoding.to_s(XML::Encoding::UTF_8) -> "UTF-8"
  *
  * Converts an encoding constant defined on the XML::Encoding
  * class to its text representation.
