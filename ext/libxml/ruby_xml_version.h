159be3f61f4de47fbc145ade88778ac0563284cf
Radu Preotiuc <radu@salsify.com>
2019-07-26 15:30:47 -0400

    Fixed field names in Schema::Element.

--- ext/libxml/ruby_xml_version.h
+++ ext/libxml/ruby_xml_version.h
@@ -1,9 +1,9 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all.*/
-#define RUBY_LIBXML_VERSION  "3.1.0.3"
-#define RUBY_LIBXML_VERNUM   3103
+#define RUBY_LIBXML_VERSION  "3.1.0.4"
+#define RUBY_LIBXML_VERNUM   3104
 #define RUBY_LIBXML_VER_MAJ   3
 #define RUBY_LIBXML_VER_MIN   1
 #define RUBY_LIBXML_VER_MIC   0
-#define RUBY_LIBXML_VER_PATCH 3
+#define RUBY_LIBXML_VER_PATCH 4
