181467290bdaaa7844a6952c59b3fa2d34c040ac
Radu Preotiuc <radu@salsify.com>
2019-08-05 11:12:51 -0400

    One more fix for the max/minOccurs issue

--- ext/libxml/ruby_xml_version.h
+++ ext/libxml/ruby_xml_version.h
@@ -1,9 +1,9 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all.*/
-#define RUBY_LIBXML_VERSION  "3.1.0.5"
-#define RUBY_LIBXML_VERNUM   3105
+#define RUBY_LIBXML_VERSION  "3.1.0.6"
+#define RUBY_LIBXML_VERNUM   3106
 #define RUBY_LIBXML_VER_MAJ   3
 #define RUBY_LIBXML_VER_MIN   1
 #define RUBY_LIBXML_VER_MIC   0
-#define RUBY_LIBXML_VER_PATCH 5
+#define RUBY_LIBXML_VER_PATCH 6
