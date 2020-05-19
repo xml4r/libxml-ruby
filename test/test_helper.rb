f40b1583283a71abf234c002a9d0723f48a544a5
Charlie Savage <cfis@savagexi.com>
2020-05-18 23:45:40 -0700

    Use bundler instead of manipulating load paths.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,13 +2,6 @@
 
 # To make testing/debugging easier, test within this source tree versus an installed gem
 
-dir = File.dirname(__FILE__)
-root = File.expand_path(File.join(dir, '..'))
-lib = File.expand_path(File.join(root, 'lib'))
-ext = File.expand_path(File.join(root, 'ext', 'libxml'))
-
-$LOAD_PATH << lib
-$LOAD_PATH << ext
-
-require 'xml'
+require 'bundler/setup'
 require 'minitest/autorun'
+require 'libxml-ruby'
