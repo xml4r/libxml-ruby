ff0535e94cabb21584c92741b7e19de631d8219f
Charlie Savage <cfis@savagexi.com>
2020-05-18 23:26:58 -0700

    Cleanup extconf.rb a bit.

--- ext/libxml/extconf.rb
+++ ext/libxml/extconf.rb
@@ -25,33 +25,37 @@ else
   dir_config('xml2')
 end
 
-unless find_header('libxml/xmlversion.h',
-                   '/opt/include/libxml2',
-                   '/opt/local/include/libxml2',
-                   '/usr/local/include/libxml2',
-                   '/usr/include/libxml2') &&
-        find_library('xml2', 'xmlParseDoc',
-                     '/opt/lib',
-                     '/opt/local/lib',
-                     '/usr/local/lib',
-                     '/usr/lib')
-    crash(<<EOL)
-need libxml2.
+found_header = find_header('libxml/xmlversion.h',
+                           '/opt/include/libxml2',
+                           '/opt/local/include/libxml2',
+                           '/usr/local/include/libxml2',
+                           '/usr/include/libxml2',
+                           '/usr/local/include')
 
-    Install the library or try one of the following options to extconf.rb:
+found_lib = find_library('xml2', 'xmlParseDoc',
+                         '/opt/lib',
+                         '/opt/local/lib',
+                         '/usr/local/lib',
+                         '/usr/lib')
 
-      --with-xml2-config=/path/to/xml2-config
-      --with-xml2-dir=/path/to/libxml2
-      --with-xml2-lib=/path/to/libxml2/lib
-      --with-xml2-include=/path/to/libxml2/include
-EOL
+found_lib ||= find_library('libxml2', 'xmlParseDoc',
+                           '/opt/lib',
+                           '/opt/local/lib',
+                           '/usr/local/lib',
+                           '/usr/lib')
+
+if !found_header || !found_lib
+    crash(<<~EOL)
+      Cannot find libxml2.
+
+      Install the library or try one of the following options to extconf.rb:
+  
+        --with-xml2-config=/path/to/xml2-config
+        --with-xml2-dir=/path/to/libxml2
+        --with-xml2-lib=/path/to/libxml2/lib
+        --with-xml2-include=/path/to/libxml2/include
+    EOL
 end
-
-have_func('rb_io_bufwrite', 'ruby/io.h')
-
-# For FreeBSD add /usr/local/include
-$INCFLAGS << " -I/usr/local/include"
-$CFLAGS << ' ' << $INCFLAGS
 
 create_header()
 create_makefile('libxml_ruby')
