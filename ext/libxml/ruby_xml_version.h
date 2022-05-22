6c8cd05f9a06facc80cbb4c0910ed014b4e40359
Charlie Savage <cfis@savagexi.com>
2022-05-22 12:58:26 -0700

    Version 3.2.3

--- ext/libxml/ruby_xml_version.h
+++ ext/libxml/ruby_xml_version.h
@@ -1,7 +1,7 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all.*/
-#define RUBY_LIBXML_VERSION  "3.2.3-dev"
+#define RUBY_LIBXML_VERSION  "3.2.3"
 #define RUBY_LIBXML_VERNUM   323
 #define RUBY_LIBXML_VER_MAJ   3
 #define RUBY_LIBXML_VER_MIN   2
