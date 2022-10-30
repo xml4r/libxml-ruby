4cc0f4c8b4a02677d59c2b826e5c675a4cad94d5
Charlie Savage <cfis@savagexi.com>
2022-10-29 18:25:26 -0700

    Try again on updating the version.

--- ext/libxml/ruby_xml_version.h
+++ ext/libxml/ruby_xml_version.h
@@ -1,8 +1,8 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all.*/
-#define RUBY_LIBXML_VERSION  "3.2.3"
-#define RUBY_LIBXML_VERNUM   323
+#define RUBY_LIBXML_VERSION  "3.2.4"
+#define RUBY_LIBXML_VERNUM   324
 #define RUBY_LIBXML_VER_MAJ   3
 #define RUBY_LIBXML_VER_MIN   2
 #define RUBY_LIBXML_VER_MIC   4
