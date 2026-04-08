a7293933f9e8aa2e125c9d09a5430fadd826e458
Charlie Savage <cfis@savagexi.com>
2026-04-07 23:52:52 -0700

    Add rdoc call-seq comments to Schema and Error classes

--- lib/libxml/schema/element.rb
+++ lib/libxml/schema/element.rb
@@ -3,22 +3,42 @@
 module LibXML
   module XML
     class Schema::Element
+      # call-seq:
+      #   element.min_occurs -> Integer
+      #
+      # Returns the minimum number of times this element must appear.
       def min_occurs
         @min
       end
 
+      # call-seq:
+      #   element.max_occurs -> Integer
+      #
+      # Returns the maximum number of times this element may appear.
       def max_occurs
         @max
       end
 
+      # call-seq:
+      #   element.required? -> true or false
+      #
+      # Returns whether this element is required (min_occurs > 0).
       def required?
         !min_occurs.zero?
       end
 
+      # call-seq:
+      #   element.array? -> true or false
+      #
+      # Returns whether this element can appear more than once (max_occurs > 1).
       def array?
         max_occurs > 1
       end
 
+      # call-seq:
+      #   element.elements -> Hash
+      #
+      # Returns the child elements of this element's type.
       def elements
         type.elements
       end
--- lib/libxml/schema/type.rb
+++ lib/libxml/schema/type.rb
@@ -1,14 +1,37 @@
 module LibXML
   module XML
     class Schema::Type
+      # call-seq:
+      #   type.kind_name -> String
+      #
+      # Returns the name of the constant that matches this type's +kind+ value.
+      #
+      #   schema.types['shiporderType'].kind_name
+      #   # => :XML_SCHEMA_TYPE_COMPLEX
       def kind_name
         Schema::Types.constants.find { |k| Schema::Types.const_get(k) == kind }
       end
 
+      # call-seq:
+      #   type.anonymous_subtypes -> Hash
+      #
+      # Returns a Hash of child elements whose types are anonymous
+      # (inline complex types with no global name).
+      #
+      #   type.anonymous_subtypes
+      #   # => {"shipto" => #<Schema::Element>, "item" => #<Schema::Element>}
       def anonymous_subtypes
         elements.select { |_, e| e.type.name.nil? }
       end
 
+      # call-seq:
+      #   type.anonymous_subtypes_recursively -> Array
+      #
+      # Returns a flattened Array of Hashes mapping qualified names to
+      # anonymous Schema::Type instances, descending into nested types.
+      #
+      #   type.anonymous_subtypes_recursively
+      #   # => [{"shipto" => #<Schema::Type>}, {"item" => #<Schema::Type>}]
       def anonymous_subtypes_recursively(parent=nil)
         anonymous_subtypes.map do |element_name, e|
           [{[parent, element_name].compact.join('::') => e.type},
