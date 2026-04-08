cb088a4397deea587e61e9639f79e4f196b9c3ee
cfis <cfis@savagexi.com>
2026-04-07 22:48:22 -0700

    Fix mispelled methods and add tests.

--- lib/libxml/schema/type.rb
+++ lib/libxml/schema/type.rb
@@ -5,14 +5,14 @@ module LibXML
         Schema::Types.constants.find { |k| Schema::Types.const_get(k) == kind }
       end
 
-      def annonymus_subtypes
+      def anonymous_subtypes
         elements.select { |_, e| e.type.name.nil? }
       end
 
-      def annonymus_subtypes_recursively(parent=nil)
-        annonymus_subtypes.map do |element_name, e|
+      def anonymous_subtypes_recursively(parent=nil)
+        anonymous_subtypes.map do |element_name, e|
           [{[parent, element_name].compact.join('::') => e.type},
-           e.type.annonymus_subtypes_recursively(element_name)]
+           e.type.anonymous_subtypes_recursively(element_name)]
         end.flatten
       end
 
